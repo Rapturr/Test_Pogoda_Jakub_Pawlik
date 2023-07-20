25cf5801ccab5fb4c646f6e64785912338dc3140
Rapturr <41922381+Rapturr@users.noreply.github.com>
2023-07-20 08:46:56 +0200

    Dodanie localStorage
    Dodawanie i wyświetlanie danych zawartych w localStorage

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 const urlForSpecificCity = `https://danepubliczne.imgw.pl/api/data/synop/station/`; 
 
+//pozyskanie danych z localStorage
+if(localStorage.getItem("info")){
+    document.getElementById('info').innerHTML = localStorage.getItem("info");
+    document.body.style.backgroundImage = localStorage.getItem("backGround");
+}
+
+//Pozyskanie danych z formularza
 const form = document.querySelector('form')
 form.addEventListener('submit', (e) => {
   e.preventDefault()
@@ -9,6 +16,7 @@ form.addEventListener('submit', (e) => {
   }
 })
 
+//Pobieranie danych z API i wyświetlanie ich na stronie
 const showInfo = (name) => {
     clearedName = name.toLowerCase().replace("ę", "e")
     .replace("ł", "l").replace("ą", "a").replace("ś", "s")
@@ -16,38 +24,41 @@ const showInfo = (name) => {
     .replace("ń", "n").replace("ó", "o").replace(/ /g,'');
     const newUrl = urlForSpecificCity+clearedName;
 
-fetch(newUrl).then((response) => {
-    return response.json();
+    fetch(newUrl).then((response) => {
+        return response.json();
+        })
+        .then((data)=>{
+            let data_pomiaru = data.data_pomiaru;
+            let temperatura = data.temperatura;
+            let suma_opadu = data.suma_opadu;
+            let cisnienie = data.cisnienie;
+            const info = 
+            `
+            <h3>Uzyskane dane miasta ${name}:</h2>
+            <p>${data_pomiaru} </p>
+            <p><img src="assets/temperatura.svg" alt="Temperatura"/>Temperatura: ${temperatura} ℃</p>
+            <p><img src="assets/opady.svg" alt="opady"/>Suma opadu: ${suma_opadu} l/m<sup>2</sup></p>
+            <p><img src="assets/cisnienie.svg" alt="cisnienie"/>Ciśnienie: ${cisnienie} hPa</p>
+            `;
+            document.getElementById('info').innerHTML = info;
+            document.body.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url('assets/${clearedName}.jpg')`;
+            
+            localStorage.clear();
+            localStorage.setItem("info",info);
+            localStorage.setItem("backGround",`linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url('assets/${clearedName}.jpg')`);
+        })
+        .catch(function(error){
+            console.log(error);
     })
-    .then((data)=>{
-        let id_stacji = data.id_stacji;
-        let data_pomiaru = data.data_pomiaru;
-        let temperatura = data.temperatura;
-        let suma_opadu = data.suma_opadu;
-        let cisnienie = data.cisnienie;
-        const info = 
-        `
-        <h3>Uzyskane dane miasta ${name}:</h2>
-        <p>${data_pomiaru} </p>
-        <p><img src="assets/temperatura.svg" alt="Temperatura"/>Temperatura: ${temperatura} ℃</p>
-        <p><img src="assets/opady.svg" alt="opady"/>Suma opadu: ${suma_opadu} l/m<sup>2</sup></p>
-        <p><img src="assets/cisnienie.svg" alt="cisnienie"/>Ciśnienie: ${cisnienie} hPa</p>
-        `;
-        document.getElementById('info').innerHTML = info;
-        document.body.style.backgroundImage = `linear-gradient(rgba(0,0,0,0.7),rgba(0,0,0,0.7)),url('assets/${clearedName}.jpg')`;
-        
-    })
-    .catch(function(error){
-        console.log(error);
-})
 }
 
+//Funkcjonalność zrobiona w celu porównania stylów, postanowiłem jednak zostawić ją jako włączenie/wyłączenie trybu ciemnego
 let style = "white";
 
 function changeStyle(){
     if(style === "white"){
         style = "black";
-        document.getElementsByClassName('container')[0].style.backgroundColor =  "rgba(0, 0, 0, 0.7)";
+        document.getElementsByClassName('container')[0].style.backgroundColor =  "rgba(0, 0, 0, 0.9)";
         document.body.style.color = "rgb(255, 255, 255)";
     }
     else{
